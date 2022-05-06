45420c95bfa516c42a13279aa1b69816df827bf0
nikita122002 <73549310+nikita122002@users.noreply.github.com>
2022-05-06 17:38:30 +0300

    lab2
    lab2

---
+++ lab2/dump_lib/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='dump_library',
+    packages=[
+        'dump_library',
+        'dump_library/parsers',
+        'dump_library/parsers/Json',
+        'dump_library/parsers/TOML',
+        'dump_library/parsers/YAML',
+    ],
+    version='1.0.0',
+    author='nikita122002',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==6.2.3'],
+    test_suite='tests',
+    scripts=['dump_library/dump_console.py']
+)
